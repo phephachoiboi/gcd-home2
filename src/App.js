c68c86317892512bf191dc93abcbbab65729ba34
phephachoiboi <duyphephon@gmail.com>
2024-10-29 10:58:21 +0700

    main slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home2Scenario from "./components/Home2Scenario";
+import BackgroundSlider from "../src/assets/images/BackgroundSlider.png";
 
 function App() {
   return (
     <div className="App">
       <div className="home2">
-        <Header />
-        <Home2Scenario />
+        <div
+          className="relative max-w-[1440px] w-full h-[918px] bg-cover object-cover z-[0]"
+          style={{ backgroundImage: `url(${BackgroundSlider})` }}
+        >
+          <Header />
+          <Home2Scenario />
+        </div>
       </div>
     </div>
   );
--- src/components/Home2Scenario.js
+++ src/components/Home2Scenario.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "../App.css";
+import MainSlider from "./MainSlider";
 
 const Home2Scenario = () => {
-  return <main className="flex flex-col m-0 p-0"></main>;
+  return (
+    <main className="flex flex-col m-0 p-0">
+      <MainSlider />
+    </main>
+  );
 };
 export default Home2Scenario;
